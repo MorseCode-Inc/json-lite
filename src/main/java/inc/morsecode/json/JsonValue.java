76bf42da49f5e3f405f737f1a6d2ccede96b30fc
Brad M <brad.m404@yahoo.com>
2016-04-23 11:34:02 -0600

    Removed javadoc comment, was causing mvn failure

--- src/main/java/inc/morsecode/json/JsonValue.java
+++ src/main/java/inc/morsecode/json/JsonValue.java
@@ -1,17 +1,6 @@
 package inc.morsecode.json;
 
 
-/**
- * 
- * &copy; MorseCode Incorporated 2015<br/>
- * =--------------------------------=<br/><pre>
- * Created: Aug 21, 2015
- *
- * Description:
- * JSON Value
- * </pre></br>
- * =--------------------------------=
- */
 public abstract class JsonValue {
 	
 	public static final String NULL= "null";
